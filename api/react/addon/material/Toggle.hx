a76040a483e058699bbb0b2a51d91cc6df248543
Thomas FETIVEAU <thomas.fetiveau.tech@gmail.com>
2016-01-20 13:22:12 +0100

    Fix Toggle

--- api/react/addon/material/Toggle.hx
+++ api/react/addon/material/Toggle.hx
@@ -1,9 +1,22 @@
 package api.react.addon.material;
 
 /*
+typedef ToggleProps = {
+    defaultToggled : Bool,
+    elementStyle : Dynamic,
+    label : Dynamic,
+    labelStyle : Dynamic,
+    labelPosition : String,
+    name : String,
+    style : Dynamic,
+    value : String
+}
+*/
+
 @:jsRequire("material-ui/lib/toggle")
 extern class Toggle
 {
+/*
 	public var defaultToggled : Bool;
 	public var elementStyle : Dynamic;
 	public var label : Dynamic;
@@ -16,19 +29,5 @@ extern class Toggle
 	public function isToggled() : Bool;
 	public function setToggled(v : Bool) : Void;
 	public function onToggle(ev : js.html.Event, toggled : Bool) : Void;
-}
 */
-import api.react.ReactComponent;
-
-typedef ToggleProps = {
-    defaultToggled : Bool,
-    elementStyle : Dynamic,
-    label : Dynamic,
-    labelStyle : Dynamic,
-    labelPosition : String,
-    name : String,
-    style : Dynamic,
-    value : String
 }
-
-extern typedef Toggle = ReactComponentOf<ToggleProps, Dynamic, Dynamic>
