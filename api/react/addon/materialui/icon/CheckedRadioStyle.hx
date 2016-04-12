7bd839c873a791669efd7f5f526a7d2d43d7996b
Thibaud-M <thibaud@melpia.com>
2016-04-12 17:51:47 +0200

    add checkbox

---
+++ api/react/addon/materialui/icon/CheckedRadioStyle.hx
@@ -0,0 +1,4 @@
+package api.react.addon.materialui.icon;
+
+@:jsRequire("material-ui/lib/svg-icons/action/check-circle")
+extern class CheckedRadioStyle { }
---
+++ api/react/addon/materialui/icon/UnCheckedRadioStyle.hx
@@ -0,0 +1,4 @@
+package api.react.addon.materialui.icon;
+
+@:jsRequire("material-ui/lib/svg-icons/toggle/radio-button-unchecked")
+extern class UnCheckedRadioStyle { }
