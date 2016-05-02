3233bdfa63f2818c2090ffecdef7cc3982cc633b
Thomas FETIVEAU <thomas.fetiveau.tech@gmail.com>
2016-05-02 11:40:29 +0200

    Add Infinite scrolling component

---
+++ api/react/addon/Infinite.hx
@@ -0,0 +1,9 @@
+package api.react.addon;
+
+/**
+Extern for [React Infinite](https://github.com/seatgeek/react-infinite).
+**/
+@:jsRequire('react-infinite')
+extern class Infinite {
+	static public function containerHeightScaleFactor(factor : Float) : Void;
+}
