ef4ae6db9ef3d5b2b26db1e50ba2d99de1f574b8
Thomas FETIVEAU <thomas.fetiveau.tech@gmail.com>
2017-09-14 17:39:41 +0200

    some React Router v4 classes

---
+++ src/lib/react/addon/router/HashRouter.hx
@@ -0,0 +1,4 @@
+package react.addon.router;
+
+@:jsRequire('react-router-dom','HashRouter')
+extern class HashRouter extends react.ReactComponent  { }
---
+++ src/lib/react/addon/router/Switch.hx
@@ -0,0 +1,4 @@
+package react.addon.router;
+
+@:jsRequire('react-router-dom','Switch')
+extern class Switch extends react.ReactComponent  { }
