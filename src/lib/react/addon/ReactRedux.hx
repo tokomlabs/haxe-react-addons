eb86065a78af83620bb01aa9a9388e1ba7d7dfe6
Thomas FETIVEAU <thomas.fetiveau.tech@gmail.com>
2017-11-06 14:02:06 +0100

    Fix ReactRedux

--- src/lib/react/addon/ReactRedux.hx
+++ src/lib/react/addon/ReactRedux.hx
@@ -5,7 +5,7 @@ Extern for [ReactRedux](https://github.com/reactjs/react-redux)
 **/
 @:jsRequire('react-redux')
 extern class ReactRedux {
-	static public function connect(? m1 : Dynamic, ? m2 : Dynamic) : Dynamic;
+	static public function connect(? m1 : Dynamic, ? m2 : Dynamic, ? m3 : Dynamic, ? opt : Dynamic) : Dynamic;
 }
 
 @:jsRequire('react-redux', 'Provider')
